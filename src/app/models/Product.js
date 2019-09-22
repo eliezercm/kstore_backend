aff275f2895ec2fe9a6dfaf2298a36d95e91a577
Eliezer Martins <eliezermarttins@gmail.com>
2019-09-22 15:41:05 -0300

    product and category model created

---
+++ src/app/models/Product.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  reference: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  code: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  promoPrice: {
+    type: Number,
+  },
+  size: {
+    type: String,
+    enum: ['PP', 'P', 'M', 'G', 'GG'],
+    required: true,
+  },
+  colorCode: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model('Product', ProductSchema);
