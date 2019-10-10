bb9839563ec2fbde5215ce14907130adebfadf49
eliezercm <eliezermarttins@gmail.com>
2019-10-10 16:59:25 -0300

    Order model created

---
+++ src/app/models/Order.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const OrderSchema = new mongoose.Schema({
+  user: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  ],
+  date: {
+    type: Date,
+    default: new Date(),
+  },
+  products: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+    },
+  ],
+  productsInfo: [
+    {
+      type: Object,
+    },
+  ],
+  status: {
+    type: String,
+    enum: ['Concluído', 'Cancelado', 'Pendente'],
+    default: 'Pendente',
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+});
+
+export default mongoose.model('Order', OrderSchema);
--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -10,7 +10,7 @@ const ProductSchema = new mongoose.Schema({
     unique: true,
     required: true,
   },
-  internal_code: {
+  internalCode: {
     type: String,
     unique: true,
     required: true,
