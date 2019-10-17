56b3e1c060c8cf255fccab556ff7cf42aa173010
eliezercm <eliezermarttins@gmail.com>
2019-10-17 14:44:40 -0300

    database connection fixed

--- src/database/index.js
+++ src/database/index.js
@@ -6,16 +6,25 @@ class Database {
   }
 
   // TODO .env
-  init() {
-    this.mongoConnection = mongoose.connect(
-      'mongodb://localhost:27017/kstore',
-      {
-        useNewUrlParser: true,
-        useFindAndModify: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-      }
-    );
+  async init() {
+    try {
+      this.mongoConnection = await mongoose.connect(
+        'mongodb://localhost:27017/kstore?authSource=admin',
+        {
+          user: 'admin',
+          pass: 'rock123',
+          auth: {
+            authdb: 'admin',
+          },
+          useNewUrlParser: true,
+          useFindAndModify: true,
+          useUnifiedTopology: true,
+          useCreateIndex: true,
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
